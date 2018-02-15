11209f29499399f98fd8477429367e5780657709
Roberto Salgado <rsalgado03@gmail.com>
2018-02-14 23:29:41 -0500

    Add initial code for Footer and Header components

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="row">
+        <nav class="navbar navbar-toggleable-md navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Admin</a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="row">
+        <nav class="navbar navbar-toggleable-md navbar-light">
+            <a class="navbar-brand" href="/">PIZZA PLANET</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Menu</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>

2496cd1031b20a47057d5a4383c288d362076617
Roberto Salgado <rsalgado03@gmail.com>
2018-02-15 00:05:55 -0500

    Add Menu.vue with initial code

---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12 col-md-6"></div>
+        <table class="table table-hover">
+            <thead class="thead-default">
+
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</template>

c93fd7b2153aed7b71c3977cb50c49a37ec797cb
Roberto Salgado <rsalgado03@gmail.com>
2018-02-19 00:39:27 -0500

    Add initial code for components for: about, contact, delivery, history and ordering

---
+++ src/components/OrderingGuide.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="card">
+        <div class="card-block">
+            <h4 class="card-title">How to Order</h4>
+            <p class="card-text">
+                Some quick example....
+            </p>
+        </div>
+    </div>
+</template>

3c039fdfe06c98392cafe8c4a79cf04dabbbf621
Roberto Salgado <rsalgado03@gmail.com>
2018-02-19 01:10:15 -0500

    Add router for home and menu sections, linked to their respective components in the header

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
     <!-- Home component (comes from `ppHome`) -->
     <div class="container">
-      <pp-admin></pp-admin>
+      <router-view></router-view>
     </div>
 
     <!-- Footer component (comes from `ppFooter`) -->
@@ -20,18 +20,11 @@
 <script>
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
-import Home from './components/Home.vue';
-import Menu from './components/Menu.vue';
-import Admin from './components/Admin.vue';
-
 
 export default {
   components: {
     ppHeader: Header,
-    ppFooter: Footer,
-    ppHome: Home,
-    ppMenu: Menu,
-    ppAdmin: Admin,
+    ppFooter: Footer
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,8 @@
         <nav class="navbar navbar-toggleable-md navbar-light">
             <a class="navbar-brand" href="/">PIZZA PLANET</a>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Menu</a>
-                </li>
+                <router-link to="/" tag="li"> <a class="nav-link">Home</a> </router-link>
+                <router-link to="/menu" tag="li"> <a class="nav-link" href="">Menu</a> </router-link>
             </ul>
         </nav>
     </header>
